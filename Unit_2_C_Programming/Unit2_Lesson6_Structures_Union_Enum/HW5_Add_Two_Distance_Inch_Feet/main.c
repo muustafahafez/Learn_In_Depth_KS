39fcb67c43b771accab285b0630796f869ffc034
mustafa hafez <meldeep10@gmail.com>
2023-03-06 21:25:18 +0200

    Add distances in feet inch system

---
+++ Unit_2_C_Programming/Unit2_Lesson6_Structures_Union_Enum/HW5_Add_Two_Distance_Inch_Feet/main.c
@@ -0,0 +1,55 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 6, 2023
+ *      Author: Mustafa Hafez
+ */
+#include <stdio.h>
+
+/*Note: 1 feet = 12  inches*/
+
+typedef struct {
+	float inch;
+	int feet;
+
+}Distance;
+
+
+int main(int argc, char **argv) {
+
+
+Distance d1,d2,sum;
+printf("Enter information of Distance-1\n");
+printf("Enter feet ");
+fflush(stdin);fflush(stdout);
+scanf("%d",&d1.feet);
+
+printf("Enter inch ");
+fflush(stdin);fflush(stdout);
+scanf("%f",&d1.inch);
+
+
+printf("Enter information of Distance-2\n");
+printf("Enter feet ");
+fflush(stdin);fflush(stdout);
+scanf("%d",&d2.feet);
+
+printf("Enter inch ");
+fflush(stdin);fflush(stdout);
+scanf("%f",&d2.inch);
+
+sum.feet=d1.feet+d2.feet;
+sum.inch=d1.inch+d2.inch;
+if(sum.inch>=12.0)
+{
+	++sum.feet;
+	sum.inch-=12.0;
+}
+printf("Sum of distances = %d' -%.1f\" ",sum.feet,sum.inch);
+
+
+	return 0;
+
+
+}
+
